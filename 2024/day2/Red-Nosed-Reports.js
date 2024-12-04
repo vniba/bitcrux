e3bdf349de8ac8812efec0f3d22474e5dd8ed352
vn!ba <itsmescorona@gmail.com>
2024-12-04 21:20:01 +0530

    advent of code 2024 day two part one completed in js ref: reddit lol

--- 2024/day2/Red-Nosed-Reports.js
+++ 2024/day2/Red-Nosed-Reports.js
@@ -12,30 +12,46 @@ function getDirname() {
 const inputFile = 'sample.txt';
 const filePath = `${getDirname()}/${inputFile}`;
 
-async function main() {
-  const fileContents = (await readFile(filePath)).toString();
-
-  const valus = [];
+function convertFileTo2DArray(fileContents) {
+  const values = [];
   fileContents.split('\n').map((val) => {
     const newVal = val.trim();
     const row = newVal
       .split(/\s/)
       .map((i) => Number.parseInt(i))
       .filter(Boolean);
-    valus.push(row);
+    values.push(row);
   });
-
-  const sucess = 0;
-
-  for (const row of valus) {
-    const is = row.every((val, i, arr) => {
-      return val !== arr[i + 1];
-    });
-    console.log(is);
-  }
-
-  // The levels are either all increasing or all decreasing.
-  // Any two adjacent levels differ by at least one and at most three.
+  return values;
 }
 
+async function main() {
+  const fileContents = (await readFile(filePath)).toString();
+
+  const valus = convertFileTo2DArray(fileContents);
+  let sucessPartOne = 0;
+  let sucessPartTwo = 0;
+
+  for (const row of valus) {
+    if (!row.length) continue;
+
+    const safePos = new Set([1, 2, 3]);
+    const safeNeg = new Set([-1, -2, -3]);
+
+    for (let index = 0; index < row.length - 1; index++) {
+      // if any other value added that must be not in the safe set so
+      safePos.add(row[index] - row[index + 1]);
+      safeNeg.add(row[index] - row[index + 1]);
+    }
+
+    if (safePos.size === 3 || safeNeg.size === 3) {
+      sucessPartOne += 1;
+    }
+  }
+
+  console.log(sucessPartOne);
+  return sucessPartOne;
+}
+// ans p1: 680
+// ans p2: 738 high
 main();
